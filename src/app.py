710a2776ca623b436a6158b907ce247c55c381d6
Pablolinari <paablolinaari@gmail.com>
2024-07-06 23:33:07 +0200

    funciona listado , falta actualizar el df en st

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-from main import *
+from main import writeinsheet
 conn = st.connection("gsheets",type=GSheetsConnection)
 data = conn.read(worksheet="Hoja 1")
 st.dataframe(data)
-
+if st.button('Actualizar datos'):
+    conn.reset
+    st.dataframe(conn.read(worksheet="Hoja 1"))
