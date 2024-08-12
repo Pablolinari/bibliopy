48c0e796ca3a6886c0f5e3dc3de5067feda3ea38
Pablo Linari Pérez <paablolinaari@gmail.com>
2024-08-12 20:48:03 +0200

    Hago independiente la hoja de streamlit y dejo de usar connection para usar siempre gspread

--- src/iosmanage.py
+++ src/iosmanage.py
@@ -1,18 +1,24 @@
 import os
-
+import psutil
+import pandas as pd
+# lista todos los archivos de un directorio y subdirectorios dando el nombre el directorio al que pertenece 
+# y el tamanio en GB
 def get_archivos(ruta_dir):
-    peliculas = []
-    disco =[]
-    tamanio=[]
+    values=[]
     auxruta_dir =ruta_dir.split('\\')[-1]
     for dir_actual ,carpetas,archivos in os.walk(ruta_dir):
         aux = dir_actual.index(auxruta_dir)
         aux = dir_actual[aux:]
         for archivo in archivos :
-            tamanio.append(os.path.getsize("%s\\%s"%(dir_actual,archivo))/(1024**3))
+            tam =os.path.getsize("%s\\%s"%(dir_actual,archivo))/(1024**3)
             archivo = archivo.split('.')[0]
-            peliculas.append(archivo)
-            disco.append(aux)
-    return [peliculas,disco,tamanio]
+            values.append([archivo,aux,tam])
+    return values
 
+#Da una lista de discos duros del sistema
+
+def obtener_disco_duro():
+    particiones = psutil.disk_partitions()
+    discos = [particion.device for particion in particiones]
+    return discos
 
--- src/prueba.py
+++ src/prueba.py
@@ -12,27 +12,30 @@ sheetid = "1ryDAAKKe2thiksaN_FVnVELmJ5e2NRuB9183FlRp78Q" ##desde d/ hasta / de l
 
 sheet = client.open_by_key(sheetid)
 
-
-#Escribe en la hoja sheet los archivos listados de dir que se le han pasado 
-#si data != None se escribe el df pasado
-
-def writesheetdata(dir,number,data=None):
-    values = get_archivos(dir)
+sheet.get_worksheet(0)
+def writeinsheet(dir,number,data=None):
+    files = get_archivos(dir)
     numrows = len(sheet.get_worksheet(number).col_values(1))
     if numrows ==0 :
         sheet.get_worksheet(number).update('A1','Peliculas')
         sheet.get_worksheet(number).update('B1','Disco')
-        sheet.get_worksheet(number).update('C1','Tamaño')
         numrows+=1
     start_row = numrows + 1
     # Preparar los datos para la actualización
-    if data is not None:
+    values = []
+    if data is None:
+        for arch, disk,tam in zip(files[0], files[1],files[2]):
+            values.append([arch,disk,tam])
+    else:
         values=data.values.tolist()
-    
+        
     sheet.get_worksheet(number).batch_update([{'range': f'A{start_row}','values': values,}])
+def getsheetdata():
+    df = pd.DataFrame(sheet.get_worksheet(0).get_all_records())
+    return df
 
-def getsheetdata(index):
-    return pd.DataFrame(sheet.get_worksheet(index).get_all_records())
+
+
 
 def selectduplicated(data):
     df = pd.DataFrame(data)
@@ -40,3 +43,4 @@ def selectduplicated(data):
     duplicados = dfp.duplicated(keep=False)
     print (duplicados)
     return df[duplicados]
+
